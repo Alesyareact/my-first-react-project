4233056a7a84104cbece4ece4f934eb444a2c088
Alesya Livanova <truealesya@gmail.com>
2021-03-18 15:24:27 +0700

    Add project files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='picture'>
+      <div className="snowMan">
+        <div className="box">
+          <div className="box0"></div>
+          <div className="box1"></div>
+          <div className="box2"></div>
+          <div className="box3"></div>
+        </div>
+      </div>
+      <div id="home" className="home">
+          <div className="roof"></div>
+          <div className="walls">
+            <div className="window">
+              <div className="one"></div>
+              <div className="two"></div>
+            </div>
+          </div>
+      </div>
+    </div>
+    
+  );
+
+}
+
+export default App;
